c14e0809fb3fe5e59f8a46e8a26099953e2925e7
Rafał Kuźnik <rafalkuznik13@gmail.com>
2026-01-16 07:21:14 +0100

    Update game.h

--- game.h
+++ game.h
@@ -1,30 +1,15 @@
-#ifndef BLACKJACK_GAME_H
-#define BLACKJACK_GAME_H
+#ifndef GAME_H
+#define GAME_H
 
-#include "deck.h"
-#include "croupier.h"
 #include "player.h"
-
+#include "croupier.h"
 
 class game {
-    player Player;
-    croupier Croupier;
-    deck Deck;
-    int bet = 0;
-    int round = 1;
-    public:
-        game();
-        void setBet(int Bet); //jak to jest w player tez to chyba nie trzeba rozpisywac tego 
-        //w game.cpp cn
-        void setUpGame();
-        int gameplay(int Bet, player &p, croupier &c, deck &d);//bedzie zwracac czy gracz chce kolejne rundy(1) czy konczy(0)
-        int compareDecks(hand& player, hand& croupier); //ja bym zrobila int gdzie np 0-to samo, 1-gracz wincyj, 2-krupier wincyj
-        int res_double(player &p, croupier &c, int Bet, int wygrana,deck &d);
-        int res_stand(player &p, croupier &c, int Bet, int wygrana,deck &d);
-        
-
-
+public:
+    void setUpGame(player &p);
+    void start(player &p, croupier &c, deck &d);
+    void displayState(player &p, croupier &c, bool hideDealer) const;
+    std::string getHint(int pSum, int dVisible) const;
 };
 
-
-#endif //BLACKJACK_GAME_H
+#endif
