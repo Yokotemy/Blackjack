4f60ade7fbeb3a51f2489b0df0890bbc74d0addc
Rafał Kuźnik <rafalkuznik13@gmail.com>
2026-01-16 07:18:58 +0100

    Update card.h

--- card.h
+++ card.h
@@ -1,27 +1,25 @@
-#ifndef BLACKJACK_CARD_H
-#define BLACKJACK_CARD_H
+#ifndef CARD_H
+#define CARD_H
 
-// pomocnicze klasy do budowy karty
-enum class Face {
-    two, three, four, five,
-    six, seven, eight, nine, ten,
-    jack, queen, king, ace};
+#include <string>
 
-enum class Color {clover, spade, heart, diamond};
+enum class Face { two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace };
+enum class Color { clover, spade, heart, diamond };
 
 class card {
     Face face;
     Color color;
-    bool visible = true; //zeby karty krupiera mogly byc zakryte
+    bool visible;
 
-    public:
-        card();
-        card(Face f, Color c, bool v = true);
-        void displayCard() const; // wyświetlanie karty
-        Face getFace() const;  // getter dla face'a
-        void show();
-        void hide();
-        bool isVisible() const; //idk co te const daja ale chat kazal to robie lol
+public:
+    card();
+    card(Face f, Color c, bool v = true);
+    void displayCard() const;
+    Face getFace() const;
+    int getVal() const;
+    void show();
+    void hide();
+    bool isVisible() const;
 };
 
-#endif //BLACKJACK_CARD_H
+#endif
