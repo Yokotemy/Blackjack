2aab5cf09f2e1718d7feba9e61b8118e878aeab8
Rafał Kuźnik <rafalkuznik13@gmail.com>
2026-01-16 07:23:44 +0100

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,25 +1,5 @@
 #include "player.h"
 
-void player::setBalance(int newBalance) {
-    balance+=newBalance;
+void player::playTurn(deck &d) {
+    // Tura gracza obsługiwana interaktywnie w game.cpp
 }
-void player::setBet(int B) {
-    balance-=B;
-}
-int player::getBalance() {
-    return balance;
-}
-
-//i ja chyba tego (stand hit doible) i tak nie uzywam nigdzie tlyko samodzielnie to robie w gameplay
-void player::setStand() {
-    stand = true;
-}
-//jak cos to ja nie mam za bardzo pojecia co robie i to moze byc zle xd 
-void player::hit(deck &D){
-    takeCard(D);
-}
-void player::setDouble(int Bet,deck &D){
-    takeCard(D);
-    setBet(Bet);
-}
-
