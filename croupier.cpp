07feb5272083b0368ce12b102fcfc9923e5c10a5
Rafał Kuźnik <rafalkuznik13@gmail.com>
2026-01-16 07:19:13 +0100

    Update croupier.cpp

--- croupier.cpp
+++ croupier.cpp
@@ -1,26 +1,8 @@
-#include <iostream>
-#include<vector>
 #include "croupier.h"
 
-//trzeba override zrobic i mi sie nie chce
-
-/*void croupier::showDeck(int round) {
-    if (round == 1) {
-        hands[0].getCard(0).displayCard();
-        std::cout << " ?" << std::endl;
-    }
-    else {
-        hands[0].displayHand();
+void croupier::playTurn(deck &d) {
+    myHand.getCard(1).show();
+    while (myHand.calculateHand() < 17) {
+        takeCard(d);
     }
 }
-void croupier::takeCard(deck& D) {
-    if (hands[0].isEmpty()) {
-        hands[0].takeCard(D.giveCard());
-        hands[0].takeCard(D.giveCard());
-    }
-    else {
-        while (hands[0].calculateHand() < 18) {
-            hands[0].takeCard(D.giveCard());
-        }
-    }
-}*/
